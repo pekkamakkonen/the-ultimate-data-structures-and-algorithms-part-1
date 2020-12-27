854848d825bfddb66b868436d70b222b3c261e2c
pekkamakkonen <pek.makkonen@gmail.com>
2020-12-27 02:11:54 +0200

    Remove item

---
+++ ConsoleApp1/ConsoleApp1/Array.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Array
+    {
+        private int[] items;
+        private int count;
+
+        public Array(int length)
+        {
+            items = new int[length];
+        }
+
+        public void Insert(int item)
+        {
+            // If the array is full, resize it
+            if (items.Length == count)
+            {
+                // Create a new array (twice the size)
+                int[] newItems = new int[count * 2];
+
+                // Copy all the existing items
+                for (int i = 0; i < count; i++)
+                    newItems[i] = items[i];
+
+                // Set "items" to this new array
+                items = newItems;
+            }
+
+            // Add the new item at the end
+            items[count++] = item;
+        }
+
+        public void RemoveAt(int index)
+        {
+            // Validate the index
+            if (index < 0 || index >= count) // 4
+                throw new IndexOutOfRangeException();
+
+            // Shift the items to the left to fill the hole
+            // [10, 20, 30, 40]
+            // index: 1
+            // 1 <- 2
+            // 2 <- 3
+            for (int i = index; i< count; i++)            
+                items[i] = items[i + 1];
+            count--;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < count; i++)
+                Console.WriteLine(items[i]);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Array numbers = new Array(3);
+            numbers.Insert(10);
+            numbers.Insert(20);
+            numbers.Insert(30);
+            numbers.Insert(40);
+            numbers.RemoveAt(4);
+            numbers.Print();
+            Console.ReadKey();
+        }
+    }
+}
